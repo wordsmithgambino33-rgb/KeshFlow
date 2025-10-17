a810509ab4fe51f76e659629841560a884a0d1c2
wordsmithgambino33-rgb <wordsmithgambino33@gmail.com>
2025-10-17 06:57:56 +0200

    Update index.ts and add web folder

--- index.ts
+++ index.ts
@@ -4,8 +4,5 @@ import 'react-native-gesture-handler'; // ensures gestures work for navigation
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// Import global CSS for web
-import './global.css';
-
 // Register App for Expo and web
 registerRootComponent(App);
